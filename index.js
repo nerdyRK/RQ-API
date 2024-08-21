3e881f411e5717f457a3e3374e9c44557dbc9892
Ravi Kumar <rav7179@gmail.com>
2024-08-21 13:10:24 +0530

    api done

---
+++ index.js
@@ -0,0 +1,38 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import { connectDB, db } from "./db.js";
+
+import salesRoutes from "./routes/sales.routes.js";
+import customerRoutes from "./routes/customer.routes.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/api/sales", salesRoutes);
+app.use("/api/customers", customerRoutes);
+
+connectDB().then(() => {
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("Server is running on port 5000");
+  });
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const collection = db.collection("shopifyOrders");
+
+    const data = await collection.find({}).toArray();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  process.exit(1);
+});
